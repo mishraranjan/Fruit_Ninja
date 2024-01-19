bc540fd59c9cad0ccd6639bbd97f4b9155862dcd
Ranjan Mishra <113978396+mishraranjan@users.noreply.github.com>
2024-01-20 02:27:24 +0530

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,17 @@
+// click on start reset button
+// are we playing?
+// yes
+// reloaad page
+// no
+// show trials left
+// change button text to "reset game"
+// 1. create a random fruit
+// 2. define a random step
+// 3. move fruit down one stop every 30 sec.
+// is fruit low?
+// no?-> repeat 
+// yes -> any trials left? repeat 1.
+// no: show game over , button text:start ga,e
+
+// slice a fruit 
+// play sound
